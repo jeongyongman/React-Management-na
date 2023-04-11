6bb35969564a3c51864a3771a0b6935752d18dbf
jeongyongman <i1683u@gmail.com>
2023-04-11 17:32:52 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
+import { Paper } from '@mui/material'
+import { Table } from '@mui/material';
+import { TableHead } from '@mui/material';
+import { TableRow } from '@mui/material';
+import { TableCell } from '@mui/material';
+import { TableBody } from '@mui/material';
+
+const styles = theme =>({
+  root: {
+    width : '100%',
+    // marginTop: theme.spacing(3),
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table:{
+    minWidth:1080,
+  }
+})
 
 const customers = [
 {
@@ -31,24 +49,25 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {
-          customers.map(c => {
-            return (
-              <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-            );
-          })
-        }
-      </div>
+      <Paper sx={{width : '100%', overflowX: "auto"}}>
+        <Table sx={{minWidth: '1080px'}}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);})}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
