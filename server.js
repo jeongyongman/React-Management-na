4292a71b82e049d9265a92761ad8d4c1a898a48c
jeongyongman <i1683u@gmail.com>
2023-05-09 12:07:22 +0900

    Add Customer Delete Module

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const upload = multer({dest: './upload'});
 
 app.get('/api/customers', (req, res) => {
   connection.query(
-    "SELECT * FROM customer",
+    "SELECT * FROM customer WHERE isDeleted = 0",
     (err, rows, fields) => {
       res.send(rows);
     }
@@ -39,7 +39,8 @@ app.get('/api/customers', (req, res) => {
 app.use("/image", express.static("./upload")); 
 
 app.post('/api/customers', upload.single('image'), (req, res) => {
-  let sql = 'INSERT INTO CUSTOMER (id,image,name,birthday,gender,job) VALUES (null,?, ?, ?, ?, ?)';
+  // let sql = 'INSERT INTO CUSTOMER (id,image,name,birthday,gender,job,createDate,isDeleted) VALUES (null,?, ?, ?, ?, ?, now(),0)';
+  let sql = 'INSERT INTO CUSTOMER VALUES (null,?, ?, ?, ?, ?, now(),0)';
   let image = '/image/' + req.file.filename;
   let name = req.body.name;
   let birthday = req.body.birthday;
@@ -55,5 +56,14 @@ app.post('/api/customers', upload.single('image'), (req, res) => {
     // console.log("입력완료***********************************************")
   })
 });
+
+app.delete('/api/customers/:id', (req, res) => {
+  let sql = "UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?";
+  let params = [req.params.id];
+  connection.query(sql, params,
+    (err, rows, fields) => {
+      res.send(rows);
+    });
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
